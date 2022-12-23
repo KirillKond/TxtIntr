f98e314668954e312e433a2f7b253e9a4fb6b440
Kirill0602 <113330070+KirillKond@users.noreply.github.com>
2022-12-23 22:17:01 +0300

    Update Calculator.cpp

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
+#include <getopt.h>
 #include <unistd.h>
 #include <cmath>
 
-
-
 using namespace std;
+
 int main(int argc, char *argv[])
 {
-if(argc == 1){
-    cout << "\nСправка\n"<< "Функции калькулятора\n" << endl << "./calculator - Для вызова справки" << endl << "./calculator -o первое число второе число - Для возведения первого числа в степень второго числа" << endl;
-    cout << "Примечани: если указать только первое число то е будет возведено в степень первого числа\n" << endl;
-    exit(0);
-}
 
+    {
+        if (argc==1) {
+            cout<<"КАЛЬКУЛЯТОР"<< endl<< "Чтобы выбрать действие, нужно ввести один из параметров:" << endl<< "-s — возведение e в степень y (Пример ввода '-s y')" << endl << "-m — возведение x в степень y (Пример ввода '-m x y')"<< endl;
+        }
 
-int nmbr = 3;
+        int opt,result,x,y;
+        float d;
+        while ((opt = getopt (argc, argv, "m:s:")) != -1) {
+            switch (opt) {
+            case 's':
+                    x = strtol(argv[2], NULL, 10);
+                    d=exp(x);
+                cout<< "Результат: "<< d <<endl;
+                break;
 
-int opt;
-while((opt = getopt(argc, argv, "a:")) != -1)
-{
-    string a = optarg;
-     
-     
-     
-     if(a == "stepenE"){
-    cout<< exp(atoi(argv[nmbr])) <<endl;
+            case 'm':
+                x = strtol(argv[2], NULL, 10);
+                y = strtol(argv[3], NULL, 10);
+                result = pow(x,y);
+                cout<< "Результат: "<< result <<endl;
+                break;
+            }
+        }
     }
-     
-     
-     
-    
-    if(a == "stepenXY"){
-    cout<< pow(atoi(argv[3]),atoi(argv[4])) <<endl;
-    }
-
-    cout<<endl;
-}
-return 0;
 }
